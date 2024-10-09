939234b0ef5251f91e95676ab26a34687f462815
Ritvij Saxena <saxenaritvij@gmail.com>
2024-10-09 00:30:58 -0400

    added more validations

--- controllers/receipt_controllers.py
+++ controllers/receipt_controllers.py
@@ -7,7 +7,14 @@ receipt_bp = Blueprint("receipts", __name__)
 
 @receipt_bp.route("/receipts/process", methods=["POST"])
 def process_receipt():
-    receipt_data = request.get_json()
+    try:
+        receipt_data = request.get_json()
+        if receipt_data is None:  # Check if JSON was not provided
+            return jsonify({"error": "Invalid request body: JSON is required."}), 400
+
+    except Exception as e:
+        return jsonify({"error": f"Invalid request body: {str(e)}"}), 400
+
     errors = validate_receipt(receipt=receipt_data)
 
     # If there are validation errors, return them in the response
